f62c9e6bb384e7afe32142f94ec95b4ec48a4ef8
Evgeniy <evgenih@il.ibm.com>
2017-03-13 09:08:33 +0200

    Adding tests

--- csb/test/com/csb/kafka/CSBTest.java
+++ csb/test/com/csb/kafka/CSBTest.java
@@ -1,11 +1,19 @@
 package com.csb.kafka;
 
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.util.Random;
 
 /**
  * Created by evgeniyh on 12/03/17.
  */
 public class CSBTest {
+    private static final int MAX_TIMEOUT = 3000;
+    private static final int DEFAULT_SLEEP = 300;
+
     private final String TEST_TOPIC = "test_topic_c8f45cb1-94e9-4138-8eea-874aa4ec7b02";
 
     @Test
@@ -20,10 +28,61 @@ public class CSBTest {
         producer.close();
         consumer.start();
 
-        while (true) {
-            if (message_received[0]) {
-                break;
+        int waited = 0;
+        while (waited < MAX_TIMEOUT) {
+            if (!message_received[0]) {
+                Thread.sleep(DEFAULT_SLEEP);
+                waited += DEFAULT_SLEEP;
             }
         }
+        Assert.assertTrue(message_received[0]);
+    }
+
+    @Test(expected = IllegalAccessError.class)
+    public void failOnStartingWithoutRegistering() throws Exception {
+        CSBConsumer consumer = new CSBConsumer();
+        consumer.start();
+    }
+
+    @Test
+    public void testTwoConsumersReceiveSameMessage() throws Exception {
+        CSBConsumer consumer1 = new CSBConsumer();
+        CSBConsumer consumer2 = new CSBConsumer();
+        CSBProducer producer = new CSBProducer();
+
+        Random r = new Random();
+        String number = String.valueOf(r.nextInt());
+        producer.sendMsg(TEST_TOPIC, number);
+
+        final Integer[] counter = new Integer[]{0};
+        consumer1.register(TEST_TOPIC, message -> {
+            if (message.equals(number)) {
+                counter[0]++;
+            }
+        });
+        consumer2.register(TEST_TOPIC, message -> {
+            if (message.equals(number)) {
+                counter[0]++;
+            }
+        });
+
+        int waited = 0;
+        while (waited < MAX_TIMEOUT) {
+            if (counter[0] == 2) {
+                Thread.sleep(DEFAULT_SLEEP);
+                waited += DEFAULT_SLEEP;
+            }
+        }
+        Assert.assertTrue(counter[0] == 2);
+    }
+
+    @BeforeClass
+    public static void setUp() {
+
+    }
+
+    @AfterClass
+    public static void cleanUp() {
+
     }
 }
