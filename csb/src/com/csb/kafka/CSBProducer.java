43a5ee424bb068002f83a41821f43c8067222005
Evgeniy <evgenih@il.ibm.com>
2017-03-14 14:05:08 +0200

    Replacing create topic

--- csb/src/com/csb/kafka/CSBProducer.java
+++ csb/src/com/csb/kafka/CSBProducer.java
@@ -1,10 +1,5 @@
 package com.csb.kafka;
 
-import kafka.admin.AdminUtils;
-import kafka.utils.ZKStringSerializer$;
-import kafka.utils.ZkUtils;
-import org.I0Itec.zkclient.ZkClient;
-import org.I0Itec.zkclient.ZkConnection;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -43,32 +38,6 @@ public class CSBProducer {
     public void sendMsgNoWait(String topic, String msg) throws Exception {
         sendMsg(topic, msg, true);
         producer.flush();
-    }
-
-    private void createTopic(String topicName) {
-        ZkClient zkClient = null;
-        ZkUtils zkUtils = null;
-        try {
-            String zookeeperHosts = "192.168.56.101:2181"; // If multiple zookeeper then -> String zookeeperHosts = "192.168.20.1:2181,192.168.20.2:2181";
-            int sessionTimeOutInMs = 15 * 1000; // 15 secs
-            int connectionTimeOutInMs = 10 * 1000; // 10 secs
-
-            zkClient = new ZkClient(zookeeperHosts, sessionTimeOutInMs, connectionTimeOutInMs, ZKStringSerializer$.MODULE$);
-            zkUtils = new ZkUtils(zkClient, new ZkConnection(zookeeperHosts), false);
-
-            int noOfPartitions = 2;
-            int noOfReplication = 3;
-            Properties topicConfiguration = new Properties();
-
-            AdminUtils.createTopic(zkUtils, topicName, noOfPartitions, noOfReplication, topicConfiguration, null);
-
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        } finally {
-            if (zkClient != null) {
-                zkClient.close();
-            }
-        }
     }
 
     public void close() {
